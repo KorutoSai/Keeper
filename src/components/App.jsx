36ec9aad771df5d1207b1408ada4ae6de77b0239
Koruto Sai <kertsayson@gmail.com>
2024-06-21 14:45:58 +0800

    keeper

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+    
+      {notes.map(noteContent => (
+        <Note
+          key={noteContent.key}
+          title={noteContent.title}
+          content={noteContent.content}
+        />
+
+      ))}
+   
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
